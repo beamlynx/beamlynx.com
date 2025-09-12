7a124189b3a07a35a185c457a531a00d8a81bef2
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2025-09-12 07:10:31 +0200

    chore: example for group

--- src/pages/documentation/Group.tsx
+++ src/pages/documentation/Group.tsx
@@ -6,8 +6,9 @@ const Group: React.FC = () => {
   const examples: DocumentationExample[] = [
     {
       title: 'Basic grouping',
-      expression: 'users | group: age => count',
-      description: 'Group users by age and count the number of users in each age group'
+      expression: "customers as c | s: email | orders as o | group: c.email => count",
+      sql: "SELECT c.email, COUNT(1) FROM customers as c JOIN orders as o ON c.id = o.customer_id GROUP BY c.email",
+      description: 'Group customers by email and count the number of orders for each customer'
     }
   ];
 
@@ -20,21 +21,6 @@ const Group: React.FC = () => {
       description="Used to group the results of a query by a given column."
       examples={examples}
     >
-      <h3>Example</h3>
-      <p>
-        If you want to group the results of the <code>users</code> table by the <code>age</code> column, you can do:
-      </p>
-      <pre>
-        <code>{`users | group: age => count`}</code>
-      </pre>
-      <p>
-        This will return the number of users in each age group.
-      </p>
-      
-      <h3>What about other functions?</h3>
-      <p>
-        This feature is new and being worked on. Your feedback is welcome.
-      </p>
     </DocumentationSection>
   );
 };
